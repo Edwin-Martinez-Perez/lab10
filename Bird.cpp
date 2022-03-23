91da795846a185062478370ea44990f848c01427
Edwin <eamartinezperez@noctrl.edu>
2022-03-23 16:41:39 -0500

    Bird.cpp, Bird.h, CMakeLists.txt, main.cpp

---
+++ Bird.cpp
@@ -0,0 +1,5 @@
+//Edwin Martinez
+// Created by Edwin on 3/23/2022.
+//Bird class file Bird.cpp
+
+#include "Bird.h"
---
+++ Bird.h
@@ -0,0 +1,14 @@
+//Edwin Martinez
+// Created by Edwin on 3/23/2022.
+//Bird class file Bird.h
+
+#ifndef LAB10_BIRD_H
+#define LAB10_BIRD_H
+
+
+class Bird {
+
+};
+
+
+#endif //LAB10_BIRD_H
